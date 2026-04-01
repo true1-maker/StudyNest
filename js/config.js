9838d1973d87c654f5b3fa99216e08a833de7d09
true1-maker <versiontrue2@gmail.com>
2026-04-01 15:53:57 +0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// =============================================
+// StudyNest — Js/config.js
+// Firebase configuration
+// =============================================
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4ceXvyKye0PaufhOTt_eeqsJvCtkQdpc",
+  authDomain: "studynest-3dbc2.firebaseapp.com",
+  projectId: "studynest-3dbc2",
+  storageBucket: "studynest-3dbc2.firebasestorage.app",
+  messagingSenderId: "456173443651",
+  appId: "1:456173443651:web:e74509981f0629531c92f6"
+};
+
